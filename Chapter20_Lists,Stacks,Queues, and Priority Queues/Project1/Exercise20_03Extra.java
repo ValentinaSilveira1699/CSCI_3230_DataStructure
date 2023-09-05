e36f5a216d5cd717b713c0dbd257ea1fa901fe2f
Valentina Silveira <114434168+ValentinaSilveira1699@users.noreply.github.com>
2023-09-05 14:06:09 -0400

    Update Exercise20_03Extra.java
    Text

--- Chapter20_Lists,Stacks,Queues, and Priority Queues/Project1/Exercise20_03Extra.java
+++ Chapter20_Lists,Stacks,Queues, and Priority Queues/Project1/Exercise20_03Extra.java
@@ -1,3 +1,18 @@
+/*
+(Perform set operations on priority queues)
+Use the template at https://liveexample.pearsoncmg.com/test/Exercise20_03Extra.txt to write a program that creates two priority queues for integers. Prompt the user to enter two lines of integers. Read each line of integers as a string and extract the integers from the string and add the integers to the two queues, respectively. Display their union, difference, and intersection. Note that the priority queue can have duplicates. The union of two priority queues may have duplicates. Display all output in increasing order.
+Here is a sample run:
+Enter integers for priority queue 1: 1 9 2 30 21 4 1
+Enter integers for priority queue 2: 12 19 2 10 12 1
+The union of the two priority queues is 
+1 1 1 2 2 4 9 10 12 12 19 21 30 
+The difference of the two priority queues is 
+4 9 21 30 
+The intersection of the two priority queues is 
+1 1 2
+Use the Collection interface’s addAll, removeAll, and retainAll methods to perforn union, difference, and intersection. 
+Class Name: Exercise20_03Extra
+*/
 import java.util.*;
 
 public class Exercise20_03Extra {
